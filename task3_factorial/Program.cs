e33405b5260dfcb797c5ca5e5c6e4de2418ef378
dmitry_di <dnd_d@mail.ru>
2022-05-06 14:59:31 +0300

    Написали программу для вычисления факториала и числа Фибоначчи с помощью рекурсии

---
+++ task3_factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿// Вычисляем факториал с помощью рекурсии
+Console.Clear();
+/*double factorial(int n)
+{
+if (n == 1) return 1;
+else return n * factorial(n - 1);
+}
+for (int i = 1; i < 5; i++)
+{
+    Console.WriteLine($"{i}! = {factorial(i)}");
+}*/
+int Fibonacci(int n)
+{
+    if(n == 1 || n==2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 42; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+} 
